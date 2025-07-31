a6144e0d950eab3dbfa0d826e3d4e3cf37ab78a7
gabriel <gsoteldor@gmail.com>
2025-07-31 15:37:15 +0200

    Comienza cpp03

---
+++ cpp03/ex03/main.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabo <gabo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 15:34:52 by gabo              #+#    #+#             */
+/*   Updated: 2025/07/31 15:34:56 by gabo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main() {
+	// This is a placeholder main function.
+	// The actual implementation will depend on the specific requirements of the project.
+	return 0;
+}
