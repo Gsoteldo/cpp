a6b857717e6d95bb92f1db4e8acf53834901acfa
Gabriel Alejandro Soteldo Rodriguez <gsoteldo@c1r10s1.42madrid.com>
2026-02-10 10:08:44 +0100

    Comprobado ex01 con test propios

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,18 +3,66 @@
 
 #include <iostream>
 
+
+
+class Awesome
+{
+	private:
+		int _n;
+	public:
+		Awesome(void): _n(42) {return;}
+
+		int	get_n() const {return (this->_n);}
+		Awesome & operator=(const Awesome& rhs);
+		bool operator==(Awesome const & rhs) const;
+		bool operator!=(Awesome const & rhs) const;
+		bool operator>(Awesome const & rhs) const;
+		bool operator<(Awesome const & rhs) const;
+		bool operator>=(Awesome const & rhs) const;
+		bool operator<=(Awesome const & rhs) const;
+
+};
+/* << overlaod */
+std::ostream & operator<<(std::ostream & os, const Awesome &a) { os << a.get_n(); return (os);}
+/* operators overloads */
+Awesome & Awesome::operator=(const Awesome& a) {this->_n = a._n; return (*this); }
+bool Awesome::operator==(Awesome const & rhs) const { return (this->_n == rhs._n); }
+bool Awesome::operator!=(Awesome const & rhs) const { return (this->_n != rhs._n); }
+bool Awesome::operator>(Awesome const & rhs) const { return (this->_n > rhs._n); }
+bool Awesome::operator<(Awesome const & rhs) const { return (this->_n < rhs._n); }
+bool Awesome::operator>=(Awesome const & rhs) const { return (this->_n >= rhs._n); }
+bool Awesome::operator<=(Awesome const & rhs) const { return (this->_n <= rhs._n); }
+
+
+
+
 template <typename T>
 void printArray(T const &value) {
 	std::cout << value <<std::endl;
 }
 
-
-
+template <typename T>
+void printer(T const &value) {
+	std::cout << value <<std::endl;
+}
 
 template <typename T>
-void iter(T *address, const int length, void(*f)(T const &)) {
+void iter(T *address, const int length, void(*f)(T &)) {
 	if (!address)
 		return ;	
+
+	for (int i = 0; i < length; i++) {
+		f(address[i]);
+	}
+}
+
+template <typename T>
+void iter(const T *address, const int length, void(*f)(const T &)) {
+	if (!address || !f)
+		return ;
+
+	if (length <= 0)
+		return ;
 
 	for (int i = 0; i < length; i++) {
 		f(address[i]);
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,7 +1,80 @@
 #include "iter.hpp"
 
+void increment(int &x) {
+	x++;
+}
+
+void increment (char &x) {
+	x++;
+}
+
+void setZero (int &x) {
+	x = 0;
+}
+
+void testPrint() {
+	int array[4] = {1, 2, 3, 4};
+	std::cout << "Print Array:" << std::endl;
+	::iter(array, 4, printArray<int>);
+	std::cout << std::endl;
+}
+
+
+void testIncrementInt() {
+	int array[4] = {1, 2, 3, 4};
+	std::cout << "Increment Values" << std::endl;
+	::iter(array, 4, increment);
+	::iter(array, 4, printArray<int>);
+	std::cout << std::endl;
+}
+
+void testSetZero() {
+	int array[4] = {1, 2, 3, 4};
+	std::cout << "Set Values to Zero" << std::endl;
+	::iter(array, 4, setZero);
+	::iter(array, 4, printArray<int>);
+
+	std::cout << std::endl;
+}
+
+void testIncrementChar() {
+	char array[5] = "Hola";
+	std::cout << "Increment Values" << std::endl;
+	::iter(array, 5, increment);
+	::iter(array, 5, printArray<char>);
+	std::cout << std::endl;
+}
+
+void testNullPointer() {
+	int *array = NULL;
+	std::cout << "Test: NULL pointer" << std::endl;
+	::iter(array, 4, printArray<int>); // No debe hacer nada, no se llama a la función de impresión
+
+
+	int array2[4] = {1, 2, 3, 4};
+	std::cout << "Test: Length zero" << std::endl;
+	::iter(array2, 0, printArray<int>); // No debe hacer nada, no se llama a la función de impresión
+
+	std::cout << std::endl;
+}
+
 int main() {
 	int array[4] = {1 ,2, 3, 4 };
 
 	::iter(array, 4 , ::printArray<int>);
+
+	//Test incrementing values
+	testIncrementInt();
+	testIncrementChar();
+
+	//Test print
+	testPrint();
+
+	//Test set to zero
+	testSetZero();
+
+	//Test null pointer
+	testNullPointer();
+
+	return (0);
 }
